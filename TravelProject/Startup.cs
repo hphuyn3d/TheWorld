72c8777003e2c94c5b9ea34e41fdae302073aacc
Hung Huynh <hung.huynh@payfactors.com>
2017-10-22 21:25:51 -0400

    Implementing a service

--- TravelProject/Startup.cs
+++ TravelProject/Startup.cs
@@ -50,6 +50,7 @@ namespace TravelProject
 
             services.AddDbContext<WorldContext>(); // Register entity framework
             services.AddScoped<IWorldRepository, WorldRepository>();
+            services.AddTransient<GeoCoordsService>();
             services.AddTransient<WorldContextSeedData>();
             services.AddLogging();
             services.AddMvc()
